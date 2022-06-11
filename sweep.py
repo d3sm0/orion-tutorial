a4cc8ede18df565f558b007bb19fbb73eebe9336
d3sm0 <simone.totaro@gmail.com>
2022-06-11 00:07:20 -0400

    [CLUSTER] teat

--- sweep.py
+++ sweep.py
@@ -1,51 +1,9 @@
-import json
 import os
-import re
-import subprocess as sp
-import sys
 from typing import Dict, Any
 
 import git
 import orion.client
 import yaml
-
-import wandb
-
-
-def get_step(name: str) -> int:
-    return int(re.compile(r"(\d+)").findall(name)[0])
-
-
-def agent(train_id, wandb_path):
-    experiment = orion.client.build_experiment(train_id)
-    while True:
-        try:
-            trial = experiment.suggest()
-        except Exception as e:
-            print(e)
-            experiment.close()
-            sys.exit(1)
-        report_path = os.path.join(experiment.working_dir, experiment.name, trial.hash_params)
-        args = ""
-        try:
-            # this a way to do checkpointing. For what i understood Orion recover the hashes of the breeded params
-            # so in order to reboot from where we are, we need to query wandb for the has param, get the checkpoint
-            # and pass it as an argument to main
-            wandb_run = wandb.Api().runs(wandb_path, filters={"^config.params_hash": trial.hash_params},
-                                         order="-created_at")[0]
-            args += f"--ckpt_base_run={wandb_run}"
-        except IndexError:
-            pass
-        os.makedirs(report_path, exist_ok=True)
-        args += " ".join([f"--{k}={v}" for k, v in trial.params.items()])
-        cmd = f"python main.py --report_path {report_path} {args}"
-        process = sp.run(cmd, shell=True)
-        print(process.returncode)
-        with open(os.path.join(report_path, "outcome.json"), "r") as f:
-            outcome = json.load(f)
-        experiment.observe(trial, outcome)
-        if experiment.is_done:
-            break
 
 
 def sweep(space: Dict[str, Any], debug=True):
@@ -59,7 +17,11 @@ def sweep(space: Dict[str, Any], debug=True):
     experiment = orion.client.build_experiment(experiment_name,
                                                space=space,
                                                debug=debug,
-                                               **orion_config
+                                               storage={
+                                                   "type": os.environ["ORION_DB_TYPE"],
+                                                   "host": os.environ["ORION_DB_ADDRESS"]
+                                               },
+                                               algorithms=orion_config["algorithms"]
                                                )
     # TODO: verify experiment exist on mongo
     return experiment.name
@@ -70,6 +32,3 @@ if __name__ == '__main__':
     with open("sweep.yaml", "r") as f:
         sweep_config = yaml.safe_load(f)
     experiment_id = sweep(sweep_config)
-    wandb_path = "d3sm0-orion"  # buddy can gives us one of this
-    # this now replace the entry point of srun_python from main.py to agent $experiment_id
-    agent(experiment_id, wandb_path)
